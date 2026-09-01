2bff3becf37b84383ea1a91e9bf69a1835d2fdc9
𝑬𝑵𝑮. 𝑬𝑳𝒀𝑨𝑺 𝑨𝑳 𝑺𝑯𝑨𝑬𝑩𝑰 <em775377979@gmail.com>
2026-09-01 06:14:59 +0300

    revert(sandbox): restore previous assessment design

--- apps/web/src/app/student/session/[id]/layout.tsx
+++ apps/web/src/app/student/session/[id]/layout.tsx
@@ -1,6 +1,5 @@
 import AssessmentLetterStimulusPreviewFix from "@/components/AssessmentLetterStimulusPreviewFix";
 import TemporaryAudioSkipControl from "@/components/TemporaryAudioSkipControl";
-import "./assessment-responsive.css";
 
 export default function StudentAssessmentLayout({ children }: { children: React.ReactNode }) {
   return (
